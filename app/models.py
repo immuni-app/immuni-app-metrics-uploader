c773b07df0d5c3c5468579359fe45b70116bcaea
Andrea Stagi <stagi.andrea@gmail.com>
2021-01-21 22:26:09 +0100

    chore: first import

---
+++ app/models.py
@@ -0,0 +1,72 @@
+import csv
+import locale
+import os
+import shutil
+
+from datetime import datetime
+from enum import Enum
+
+
+locale.setlocale(locale.LC_TIME, "")
+
+
+class Platform(Enum):
+    IOS = "IOS"
+    ANDROID = "ANDROID"
+
+
+class MetricsException(Exception):
+    pass
+
+
+class Metrics:
+    def __init__(self, csv_file_path: str):
+        self._csv_file_path: str = csv_file_path
+        try:
+            self._parse_csv_file()
+        except UnicodeDecodeError:
+            raise MetricsException("Unable to read the file")
+        except FileNotFoundError:
+            raise MetricsException("Unable to find the file")
+
+    def _parse_csv_file(self) -> None:
+        with open(self._csv_file_path) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=",")
+            line_count: int = 0
+            row: list = []
+            for row in csv_reader:
+                if line_count == 0:
+                    self._platform: Platform = self._detect_platform(row[0])
+                line_count += 1
+            if not row:
+                raise MetricsException("File is empty")
+            self._report_date: str = self._format_date(row[0])
+
+    def _detect_platform(self, cell: str) -> None:
+        if cell == "Nome":
+            return Platform.IOS
+        elif cell == "Data":
+            return Platform.ANDROID
+
+    def _format_date(self, date: str) -> str:
+        date_time_obj: datetime
+        try:
+            if self.platform == Platform.IOS:
+                date_time_obj = datetime.strptime(date, "%d/%m/%y")
+            elif self.platform == Platform.ANDROID:
+                date_time_obj = datetime.strptime(date, "%d %b %Y")
+            return date_time_obj.strftime("%d%m%Y")
+        except ValueError:
+            raise MetricsException("Error formatting date")
+
+    @property
+    def platform(self) -> Platform:
+        return self._platform
+
+    @property
+    def report_date(self) -> str:
+        return self._report_date
+
+    def export(self, destination) -> None:
+        exported_filename: str = f"IMMUNI_{self.platform.value}_{self.report_date}.csv"
+        shutil.move(self._csv_file_path, os.path.join(destination, exported_filename))
---
+++ tests/test_models.py
@@ -0,0 +1,37 @@
+import os
+import pytest
+import shutil
+
+from app.models import Metrics, MetricsException
+
+
+DATA_FOLDER: str = os.path.join(".", "tests")
+METRICS_SAMPLE_FOLDER: str = os.path.join(DATA_FOLDER, "metrics_sample")
+METRICS_DEST_FOLDER: str = os.path.join(DATA_FOLDER, "metrics")
+METRICS_PROCESSED_FOLDER: str = os.path.join(DATA_FOLDER, "metrics_processed")
+
+shutil.rmtree(METRICS_DEST_FOLDER)
+shutil.rmtree(METRICS_PROCESSED_FOLDER)
+os.mkdir(METRICS_PROCESSED_FOLDER)
+
+shutil.copytree(METRICS_SAMPLE_FOLDER, METRICS_DEST_FOLDER)
+
+
+def test_metrics():
+    csv_file: str = os.path.join(METRICS_DEST_FOLDER, "android.csv")
+    Metrics(csv_file).export(os.path.join(METRICS_PROCESSED_FOLDER))
+
+    csv_file: str = os.path.join(METRICS_DEST_FOLDER, "ios.csv")
+    Metrics(csv_file).export(os.path.join(METRICS_PROCESSED_FOLDER))
+
+    with pytest.raises(MetricsException):
+        csv_file: str = os.path.join(METRICS_DEST_FOLDER, "notexistent")
+        Metrics(csv_file).export(os.path.join(METRICS_PROCESSED_FOLDER))
+
+    with pytest.raises(MetricsException):
+        csv_file: str = os.path.join(METRICS_DEST_FOLDER, "empty.csv")
+        Metrics(csv_file).export(os.path.join(METRICS_PROCESSED_FOLDER))
+
+    with pytest.raises(MetricsException):
+        csv_file: str = os.path.join(METRICS_DEST_FOLDER, "invalid.csv")
+        Metrics(csv_file).export(os.path.join(METRICS_PROCESSED_FOLDER))
